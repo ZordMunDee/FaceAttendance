aaa36fa76ed0acbfe8bc03c5178008aadd531ff7
ZordMunDee <zeroznice.nice@gmail.com>
2026-05-11 13:50:03 +0700

    fix front ver.3

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ import { LogIn, Crown, Medal } from "lucide-react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 export default function HomePage() {
   const [time, setTime] = useState<Date | null>(null);
