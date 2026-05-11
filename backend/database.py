d61382039cb1973dab6c0c7ff300c816cd27f0db
ZordMunDee <zeroznice.nice@gmail.com>
2026-05-11 11:50:15 +0700

    feat: แก้ให้ดี UX/UXตรง Dashboard และ Backend และเพิ่ม Docker

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,20 @@
 # backend/database.py
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 1. กำหนดชื่อและที่อยู่ของไฟล์ Database
 # "sqlite:///./attendance.db" หมายถึงให้สร้างไฟล์ชื่อ attendance.db ไว้ในโฟลเดอร์เดียวกัน
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:123456@db:3306/face_attendance"
+
+DATABASE_URL = os.getenv("MYSQL_URL", "mysql+pymysql://root:123456@db:3306/face_attendance")
+
+if DATABASE_URL.startswith("mysql://"):
+    DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
+
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
